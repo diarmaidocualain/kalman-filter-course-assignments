72f862e870dec979f39070280f96af2e37ae083c
Diarmaid O Cualain <diarmaid.ocualain@xperi.com>
2021-02-04 14:12:33 +0000

    Remove time bug from assignment 4
    Signed-off-by: Diarmaid O Cualain <diarmaid.ocualain@xperi.com>

--- sim/sim2d_prediction.py
+++ sim/sim2d_prediction.py
@@ -7,7 +7,8 @@ import time
 
 
 def sim_run(options, KalmanFilter, light_state):
-    start = time.clock()
+    start = time.perf_counter()
+    # start = time.clock()
     # Simulator Options
     FIG_SIZE = options['FIG_SIZE'] # [Width, Height]
     ALLOW_SPEEDING = options['ALLOW_SPEEDING']
@@ -169,7 +170,8 @@ def sim_run(options, KalmanFilter, light_state):
         return car, light
 
 
-    print("Compute Time: ", round(time.clock() - start, 3), "seconds.")
+    print("Compute Time: ", round(time.perf_counter() - start, 3), "seconds.")
+
     # Animation.
     car_ani = animation.FuncAnimation(fig, update_plot, frames=range(1,len(t)),
                                     interval=100, repeat=False, blit=False)
